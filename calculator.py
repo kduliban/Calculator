3c7b5847b043117d07f21b332a35a78e6a15e923
kasiaduliban@gmail.com <kasiaduliban@gmail.com>
2022-03-28 23:00:40 +0200

    Create variables

---
+++ calculator.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+intro = input ("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+calc_result = None
+num_1 = float(input("Podaj składnik 1. "))
+num_2 = float(input("Podaj składnik 2. "))
+
